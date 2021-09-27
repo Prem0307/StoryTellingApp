3ebc7985482bea391a5a41764ec35276142f99b6
Prem0307 <76106328+Prem0307@users.noreply.github.com>
2021-09-27 13:51:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+ export const firebaseConfig = {
+  apiKey: "AIzaSyC7-FrQ7AD2oi4D9ZUd_pLpoG7m3Rvi77Y",
+  authDomain: "storytelling-dfb9b.firebaseapp.com",
+  databaseURL: "https://storytelling-dfb9b-default-rtdb.firebaseio.com",
+  projectId: "storytelling-dfb9b",
+  storageBucket: "storytelling-dfb9b.appspot.com",
+  messagingSenderId: "586266562363",
+  appId: "1:586266562363:web:3c097fc90c43a315b3b11e"
+};
